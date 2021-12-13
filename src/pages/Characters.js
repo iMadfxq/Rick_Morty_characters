74115b465c88b0ee1ddda0c8f2194feb2790bf17
iMadfxq <oscarfelipe123456789@gmail.com>
2021-12-13 14:15:34 -0600

    Almost done with styles for mobile and desktop

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -5,20 +5,22 @@ const Characters = async () => {
   const id = getHash()
   const character = await getData(id)
   const view = `
-    <div class="Character__inner">
-      <article class="character__inner--card">
-        <img src="${character.image}" alt="${character.name}">
-        <h2>${character.name}</h2>
-      </article>
-      <article class="character__inner--card">
-        <h3>Episodes: <span>${character.episode.length}</span></h3>
-        <h3>Status: <span>${character.status}</span></h3>
-        <h3>Specie: <span>${character.species}</span></h3>
-        <h3>Gender: <span>${character.gender}</span></h3>
-        <h3>Origin: <span>${character.origin.name}</span></h3>
-        <h3>Last location: <span>${character.location.name}</span></h3>
-      </article>
-    </div>
+    <section class="character__info">
+      <div class="character__inner">
+        <article class="character__inner--card">
+          <img src="${character.image}" alt="${character.name}">
+          <h2>${character.name}</h2>
+        </article>
+        <article class="character__inner--text">
+          <h3>Episodes: <span>${character.episode.length}</span></h3>
+          <h3>Status: <span>${character.status}</span></h3>
+          <h3>Specie: <span>${character.species}</span></h3>
+          <h3>Gender: <span>${character.gender}</span></h3>
+          <h3>Origin: <span>${character.origin.name}</span></h3>
+          <h3>Last location: <span>${character.location.name}</span></h3>
+        </article>
+      </div>
+    </section>
   `;
   return view;
 }
