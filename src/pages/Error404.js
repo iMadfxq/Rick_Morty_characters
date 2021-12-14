c782a307280103468d3c200e42c80261eedd8f6c
iMadfxq <oscarfelipe123456789@gmail.com>
2021-12-14 15:23:46 -0600

    Stylized 404 page

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -2,6 +2,7 @@ const Error404 = () => {
   const view = `
     <div class="error404">
       <h2>Error 404</h2>
+      <p>Sorry, we couldn't find what you were looking for. <a href="#">You can go to our main page again.</a></p>
     </div>
   `
   return view
